bdf8059581e6a48bbdf1d6d5694492cdc85a81f7
OlenaStoliarova <stoliarova.elena@gmail.com>
2020-02-12 00:20:41 +0200

    04 implemented method for pagination in JDBCAbstractDao

--- src/main/java/ua/training/cruise_company_servlet/dao/implementation/JDBCAbstractDao.java
+++ src/main/java/ua/training/cruise_company_servlet/dao/implementation/JDBCAbstractDao.java
@@ -3,6 +3,8 @@ package ua.training.cruise_company_servlet.dao.implementation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ua.training.cruise_company_servlet.dao.DAOLevelException;
+import ua.training.cruise_company_servlet.utility.Page;
+import ua.training.cruise_company_servlet.utility.PaginationSettings;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -13,6 +15,8 @@ public abstract class JDBCAbstractDao<T>{
     private static final Logger LOG = LogManager.getLogger(JDBCAbstractDao.class);
 
     protected static final String COLUMN_ID = "id";
+
+    private static final String SELECT_COUNT = "SELECT COUNT(*) as total_elements ";
 
     protected boolean executeCUDQuery(String query, StatementMapper statementMapper) {
         try(Connection connection = ConnectionPoolHolder.getConnection();
@@ -69,5 +73,35 @@ public abstract class JDBCAbstractDao<T>{
         }
 
         return resultList;
+    }
+
+    protected Page<T> selectMany(String query, StatementMapper statementMapper, ObjectMapper<T> objectMapper, PaginationSettings paginationSettings) {
+
+        long totalRowsInResultSet = 0;
+        String countQuery = SELECT_COUNT + query.replaceFirst(".*FROM", "FROM");
+        LOG.debug("executeQuery: " + countQuery);
+        try (Connection connection = ConnectionPoolHolder.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(countQuery)){
+
+            statementMapper.map(preparedStatement);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    totalRowsInResultSet = resultSet.getLong("total_elements");
+                }
+            }
+        } catch (SQLException e) {
+            LOG.error(e.getMessage(), e);
+            throw new DAOLevelException(e);
+        }
+
+        long offset = (paginationSettings.getCurrentPageNumber()-1) * paginationSettings.getPageSize();
+        String queryWithLimitAndOffset = query + " LIMIT " + offset + ", " + paginationSettings.getPageSize();
+        LOG.debug("queryWithLimitAndOffset: " + queryWithLimitAndOffset);
+
+        Page<T> resultPage = new Page<>(paginationSettings);
+        resultPage.setTotalElements(totalRowsInResultSet);
+        resultPage.setContent( selectMany(queryWithLimitAndOffset, statementMapper,objectMapper));
+
+        return resultPage;
     }
 }
