afc16fc5bfea1d9fe670c6ecaac3e268c1790745
OlenaStoliarova <stoliarova.elena@gmail.com>
2020-02-12 14:58:04 +0200

    added ShipService and DTOs for Ship and Extra

---
+++ src/main/java/ua/training/cruise_company_servlet/service/ShipService.java
@@ -0,0 +1,43 @@
+package ua.training.cruise_company_servlet.service;
+
+import ua.training.cruise_company_servlet.dao.DaoFactory;
+import ua.training.cruise_company_servlet.dao.ExtraDao;
+import ua.training.cruise_company_servlet.dao.SeaportDao;
+import ua.training.cruise_company_servlet.dao.ShipDao;
+import ua.training.cruise_company_servlet.entity.Extra;
+import ua.training.cruise_company_servlet.entity.Seaport;
+import ua.training.cruise_company_servlet.entity.Ship;
+import ua.training.cruise_company_servlet.web.dto.ShipDTO;
+import ua.training.cruise_company_servlet.web.dto.converter.ShipDTOConverter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ShipService {
+    private final ShipDao shipDao = DaoFactory.getInstance().createShipDao();
+    private final ExtraDao extraDao = DaoFactory.getInstance().createExtraDao();
+    private final SeaportDao seaportDao = DaoFactory.getInstance().createSeaportDao();
+
+    public List<ShipDTO> getAllShips() {
+        List<Ship> ships= shipDao.findAll();
+        for(Ship s : ships){
+            loadShipExtras(s);
+            loadSeaportsOfShipRoute(s);
+        }
+
+        return ships.stream()
+                .map(ShipDTOConverter::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    private void loadSeaportsOfShipRoute(Ship ship){
+        List<Seaport> seaports = seaportDao.findAllOfShipRoute(ship.getId());
+        ship.setVisitingPorts(new HashSet<>(seaports));
+    }
+
+    private void loadShipExtras(Ship ship){
+        List<Extra> extras = extraDao.findAllByShipId(ship.getId());
+        ship.setExtras(new HashSet<>(extras));
+    }
+}
---
+++ src/main/java/ua/training/cruise_company_servlet/web/dto/converter/ShipDTOConverter.java
@@ -0,0 +1,47 @@
+package ua.training.cruise_company_servlet.web.dto.converter;
+
+import ua.training.cruise_company_servlet.entity.Ship;
+import ua.training.cruise_company_servlet.web.dto.ExtraDTO;
+import ua.training.cruise_company_servlet.web.dto.SeaportDTO;
+import ua.training.cruise_company_servlet.web.dto.ShipDTO;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class ShipDTOConverter {
+
+    public static ShipDTO convertToDTO(Ship ship) {
+        ShipDTO dto = new ShipDTO();
+
+        dto.setId( ship.getId() );
+        dto.setName( ship.getName() );
+        dto.setCapacity( ship.getCapacity() );
+        dto.setOneTripDurationDays( ship.getOneTripDurationDays() );
+
+        setLocaleSpecificFields(dto, ship);
+
+        List<ExtraDTO> extras = ship.getExtras().stream()
+                .map(ExtraDTOConverter::convertToDTO)
+                .collect(Collectors.toList());
+        dto.setExtras( extras );
+
+        List<SeaportDTO> seaports = ship.getVisitingPorts().stream()
+                .map(SeaportDTOConverter::convertToDTO)
+                .collect(Collectors.toList());
+        dto.setVisitingPorts( seaports );
+
+        return dto;
+    }
+
+    private static void setLocaleSpecificFields(ShipDTO dto, Ship ship){
+        Locale currentLocale = Locale.getDefault();
+
+        if(currentLocale.getLanguage().equalsIgnoreCase("uk")){
+            dto.setRouteName( ship.getRouteNameUkr());
+            return;
+        }
+
+        dto.setRouteName( ship.getRouteNameEn());
+    }
+}
